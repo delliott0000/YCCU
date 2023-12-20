420a1445f0d8254c3e23d481d10e4633e2580733
Delliott <dlelliott100@outlook.com>
2023-12-20 17:06:28 +0000

    modlog not found error implementation

--- core/errors.py
+++ core/errors.py
@@ -1,3 +1,10 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+
+
 class DurationError(Exception):
 
     def __init__(self, duration: str, /) -> None:
@@ -5,3 +12,13 @@ class DurationError(Exception):
 
     def __str__(self) -> str:
         return f'`{self.duration}` is not a valid duration, please try again.'
+
+
+class ModlogNotFound(Exception):
+
+    def __init__(self, **kwargs: Any) -> None:
+        self.kwargs: dict[str, Any] = kwargs.copy()
+
+    def __str__(self) -> str:
+        formatted_kwargs = ', '.join(f'{key}={value}' for key, value in self.kwargs.items())
+        return f'No modlogs matching the following search parameters were found: `{formatted_kwargs}`'
