16315309521c1ca114da1cb1313f2db027f6023d
Delliott <dlelliott100@outlook.com>
2023-12-21 04:37:23 +0000

    modlogs/moderation cogs added

---
+++ ext/moderation.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from discord.ext import commands
+
+if TYPE_CHECKING:
+    from core.bot import CustomBot, CustomContext
+
+
+class ModerationCommands(commands.Cog):
+
+    ...
+
+
+async def setup(bot: CustomBot) -> None:
+    await bot.add_cog(ModerationCommands())
