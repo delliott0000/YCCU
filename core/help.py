2ea14782407ede1f1acb4cd87203ebffc92024a0
Delliott <dlelliott100@outlook.com>
2023-12-22 05:27:42 +0000

    help command context typing

--- core/help.py
+++ core/help.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 from discord.ext import commands
+
+if TYPE_CHECKING:
+    from core.bot import CustomContext
 
 
 class CustomHelpCommand(commands.HelpCommand):
 
-    ...
+    context: CustomContext
